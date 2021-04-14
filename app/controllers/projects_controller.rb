e90dce9d5a1c654c43b7d6e5eac67e3254e491f7
Adeel <adeel@devsinc.com>
2021-04-14 18:20:11 +0500

    Worked on Projects Controller
    * ProjectsController: create basic controller and 7 actions
    * User: Reset password/ routes

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,49 @@
 class ProjectsController < ApplicationController
+  def index
+    @projects = Project.all
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      redirect_to @project
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @project = Project.find_by(id: params[:id])
+  end
+
+  def edit
+    @project = Project.find_by(id: params[:id])
+  end
+
+  def update
+    @project = Project.find_by(id: params[:id])
+
+    if @project.update(project_params)
+      redirect_to @project
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @project = Project.find_by(id: params[:id])
+    @project.destroy
+
+    redirect_to projects_path
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:title)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users,  skip: :passwords,
-                      path_names: { sign_in: 'login', sign_out: 'logout' }
+  devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout' }
 
-  resources :projects do
-    resources :bugs
-  end
+  resources :projects
 end
